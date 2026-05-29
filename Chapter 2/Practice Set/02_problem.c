70c233a814975739e9989eca44d2b9efe33fceeb
Suvro Kayal <suvrokayal2005@gmail.com>
2026-05-29 20:34:28 +0530

    add chapter 2 practice set

---
+++ Chapter 2/Practice Set/02_problem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+2. What data type will 3.0/8 – 2 return?
+*/
+int main() {
+    float a= 3.0/8 - 2;
+    printf("%f", a);
+    return 0;
+}
---
+++ Chapter 2/Practice Set/03_problem.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//Write a program to check whether a number is divisible by 97 or not. 
+int main() {
+    int a = 2345285;
+    printf("%d", a%97);
+    return 0;
+}
